9f8ec3542e49c4efa157074cc4462787f3d016e2
luketorv <104324965+luketorv@users.noreply.github.com>
2022-09-17 17:47:42 -0500

    Updates

---
+++ precache-manifest.8b4b36d25ae9b0c0b4b31e81097ce8d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f5a3af91ffc8e91456c",
+    "url": "/luketorv/react-portfolio2/static/css/main.2b3ced10.chunk.css"
+  },
+  {
+    "revision": "7f5a3af91ffc8e91456c",
+    "url": "/luketorv/react-portfolio2/static/js/main.7f5a3af9.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/luketorv/react-portfolio2/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "29b71e7f3736fcd9450e",
+    "url": "/luketorv/react-portfolio2/static/js/2.29b71e7f.chunk.js"
+  },
+  {
+    "revision": "12207862e251f1ee681b",
+    "url": "/luketorv/react-portfolio2/static/js/runtime~main.12207862.js"
+  },
+  {
+    "revision": "d9993b56782e79446f627fe564142d35",
+    "url": "/luketorv/react-portfolio2/static/media/cover-image.d9993b56.jpg"
+  },
+  {
+    "revision": "df3af536cabfcb4a5e665892509e5087",
+    "url": "/luketorv/react-portfolio2/static/media/1songsuggest1.df3af536.jpg"
+  },
+  {
+    "revision": "afcde56c16377e5cb2f4aec975f0f665",
+    "url": "/luketorv/react-portfolio2/static/media/resume33.afcde56c.pdf"
+  },
+  {
+    "revision": "4342c1ae58efb4f3881c18bb3153899c",
+    "url": "/luketorv/react-portfolio2/index.html"
+  }
+];
